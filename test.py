bd3f1e9d443e50ff0f33b9ab3217f60e01d7a4cf
Simon <Simon.DIckreuter@uni-tuebingen.de>
2015-05-07 13:11:22 +0200

    simplified example that shows problems with gtk and python-seabreeze

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 __author__ = 'dunkelfeld'
 
 import time
-import seabreeze.spectrometers as sb
 from gi.repository import Gtk
-
+import seabreeze.spectrometers as sb
 
 devices = sb.list_devices()
 s = sb.Spectrometer(devices[0])
-s.tec_set_temperature_C(-17)
-s.tec_set_enable(True)
-for i in range(10) :
-    print(s.tec_get_temperature_C())
-    time.sleep(0.3)
-
-s.integration_time_micros(100000)
 print(s.intensities())
 print(s.wavelengths())
